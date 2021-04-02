12ca1ba1c693654eddcbe14b5dc20bfec05b1f37
Campagne_Alexandre <campagne.a@hotmail.fr>
2021-04-02 08:39:40 +0200

    mise a jour de la boutique

--- pages/boutique.php
+++ pages/boutique.php
@@ -2,7 +2,7 @@
 include 'header.php';
 ?>
 <div class="row">
-    <div class="col-md-3 text-center border-end border-bottom border-2 border-danger back-image text-white">
+    <div class="col-md-3 text-center border-end border-bottom border-2 border-danger back-image text-white position-fixed" height="100%">
         <h2 class="pt-5 pb-3">Liste de la Boutique</h2>
         <div class="dropdown-divider pt-3"></div>
         <div class="dropdown-divider pb-3"></div>
@@ -26,63 +26,81 @@ include 'header.php';
             <h3>Figurines</h3>
         </a>
     </div>
+</div>
+<div class="row">
+    <div class="col-3"></div>
     <div class="col-md card-group mt-2 me-3">
         <div class="card">
-            <img src="../Logo/Fond_de_site.jpg" class="card-img-top" alt="...">
+            <img src="../jeux_covers/51HlW5jBN-L._AC_.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">FINAL FANTASY XVI</h5>
-                <p class="card-text">Final Fantasy XVI en vente dans votre boutique pour 69.90€ !!!</p>
+                <h5 class="card-title">FINAL FANTASY XV "ROYAL EDITION"</h5>
+                <p class="card-text">Final Fantasy XV "Royal Edition" en vente dans notre boutique pour 69.90€ !!!</p>
                 <button type="button" class="btn btn-sm btn-danger">Ajouter au Panier</button>
                 <button type="button" class="btn btn-sm btn-danger float-end">69.90€</button>
             </div>
         </div>
         <div class="card">
-            <img src="../LOGO-FF/ff2.jpg" class="card-img-top" alt="...">
+            <img src="../jeux_covers/A1GoGM6suxL._AC_SX385_.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <h5 class="card-title">FINAL FANTASY XIV ONLINE</h5>
+                <p class="card-text">Final Fantasy XVI Online (A realm Reborn , HeavenSward, StrormBlood) avec un abonnement de 30 jours gratuit, en ventes dans notre boutique pour 16,90€ !!!</p>
                 <button type="button" class="btn btn-sm btn-danger">Ajouter au Panier</button>
-                <button type="button" class="btn btn-sm btn-danger float-end">20€</button>
+                <button type="button" class="btn btn-sm btn-danger float-end">16.90€</button>
             </div>
         </div>
         <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="../jeux_covers/Final_Fantasy_XIII_cover.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <h5 class="card-title">FINAL FANTASY XIII</h5>
+                <p class="card-text">Final Fantasy XIII en ventes dans notre boutique pour 50€ !!!</p>
                 <button type="button" class="btn btn-sm btn-danger">Ajouter au Panier</button>
-                <button type="button" class="btn btn-sm btn-danger float-end">0€</button>
+                <button type="button" class="btn btn-sm btn-danger float-end">50€</button>
             </div>
         </div>
         <div class="card-group">
             <div class="card">
-                <img src="../Logo/Fond_de_site.jpg" class="card-img-top" alt="...">
+                <img src="../jeux_covers/ff132.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">FINAL FANTASY XIII-2</h5>
+                    <p class="card-text">Final Fantasy XIII-2 en ventes dans notre boutique pour 50€ !!!</p>
                     <button type="button" class="btn btn-sm btn-danger">Ajouter au Panier</button>
-                    <button type="button" class="btn btn-sm btn-danger float-end">69.90€</button>
+                    <button type="button" class="btn btn-sm btn-danger float-end">50€</button>
                 </div>
             </div>
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="../jeux_covers/jaquette.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="card-title">FINAL FANTASY XIII LIGHTNING RETURNS</h5>
+                    <p class="card-text">Final Fantasy XIII "Lightning Returns" en ventes dans notre boutique pour 50€ !!!</p>
                     <button type="button" class="btn btn-sm btn-danger">Ajouter au Panier</button>
-                    <button type="button" class="btn btn-sm btn-danger float-end">0€</button>
+                    <button type="button" class="btn btn-sm btn-danger float-end">50€</button>
                 </div>
             </div>
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="../jeux_covers/ff12+cover.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    <h5 class="card-title">FINAL FANTASY XII THE ZODIAC AGE</h5>
+                    <p class="card-text">Final Fantasy XII "The Zodiac Age" en ventes dans notre boutique pour 35€ !!!</p>
                     <button type="button" class="btn btn-sm btn-danger">Ajouter au Panier</button>
-                    <button type="button" class="btn btn-sm btn-danger float-end">0€</button>
+                    <button type="button" class="btn btn-sm btn-danger float-end">35€</button>
                 </div>
             </div>
         </div>
+        <nav aria-label="...">
+            <ul class="pagination">
+                <li class="page-item">
+                    <span class="page-link">Previous</span>
+                </li>
+                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                <li class="page-item" aria-current="page">
+                    <span class="page-link">2</span>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </div>
 </body>
