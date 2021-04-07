71dd5a3d60d8497125f3aa9b09ad90d6ed6c4256
Vincent <vincentdecock@outlook.fr>
2021-04-07 20:14:29 +0100

    Utilisation de grid pour la page jeux

---
+++ pages/ff10.php
@@ -0,0 +1,130 @@
+<?php
+    include 'header.php';
+?>
+
+<link rel="stylesheet" href="styles.css"> 
+
+    <div class="container-global">
+
+
+        <div class=container-history> 
+
+            <div class="section-title-history"> 
+                This is my story
+            </div>
+
+            <div class="img-container-history">
+                <img src="..\photos en tt genre\Final_Fantasy_X.jpg" alt="picture-ff10" class="picture-ff10">
+            </div>
+
+            <div class="history-txt">
+                <p>Lorem ipsum est simplement du faux texte employé dans la composition et la mise en page avant
+                    impression.
+                    Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur
+                    anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.
+                    Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans
+                    que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles
+                    Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des
+                    applications de mise en page de texte, comme Aldus PageMaker.
+                </p>
+            </div>
+        </div>
+
+
+        <div class="sections">                          <!--  SECTION 1 -->
+            <div class="section-title">PERSONNAGES PRINCIPAUX</div>
+
+            <div class="container2">
+                <div class="sub-section">               <!-- Tidus -->
+                    <div class="tidus-txt">
+                        Tidus   
+                    </div>
+                    <div class=sub-section-img> 
+                        <img src="..\photos en tt genre\tidus2.png" alt="Tidus" class= "img-tidus" />
+                    </div>
+                    <div class="sub-section-txt">
+                        <p>Lorem ipsum est simplement du faux texte employé dans la composition et la mise en page avant
+                            impression.Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur
+                            anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.
+                            Il
+                            n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans
+                            que
+                            son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles
+                            Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des
+                            applications de mise en page de texte, comme Aldus PageMaker.
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            
+
+                <div class="sub-section">                   <!-- Yuna -->
+                <div class="yuna-txt">
+                        Yuna   
+                    </div>
+                    <div class=sub-section-img>
+                        <img src="..\photos en tt genre\yuna2.png" alt="Yuna" class="img-yuna" />
+                    </div>
+                    <div class="sub-section-txt">
+                        <p>Lorem ipsum est simplement du faux texte employé dans la composition et la mise en page avant
+                            impression.
+                            Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur
+                            anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.
+                            Il
+                            n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans
+                            que
+                            son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles
+                            Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des
+                            applications de mise en page de texte, comme Aldus PageMaker.
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="divider"></div>
+
+        <div class="sections gameplay">                          <!--  SECTION 2 -->
+            <div class="section-title">GAMEPLAY</div>
+            <div class="section-title2">Dans la continuité du tour par tour !</div>
+            
+            <div class="container2">
+                <div class="section-img-container">
+                    <img src="..\photos en tt genre\ffx-overdrive.jpg" class="img-lulu" />
+                </div>
+                <div class="gameplay-txt">
+                    <p>Lorem ipsum est simplement du faux texte employé dans la composition et la mise en page avant
+                        impression.
+                        Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur
+                        anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.
+                        Il
+                        n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans
+                        que
+                        son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles
+                        Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des
+                        applications de mise en page de texte, comme Aldus PageMaker.
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+
+        <div class="divider"></div>
+
+        <div class="container-quizz">
+            <div class="section-title-quizz">
+                En route vers le quizz !
+            </div>
+
+            <div class="img-container-quizz">
+                <img src="..\photos en tt genre\inter-ff.jpg" alt="logo-of-quizz" class="img-quizz">
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
--- pages/jeux.php
+++ pages/jeux.php
@@ -1,5 +1,117 @@
 <?php
     include 'header.php';
 ?>
+
+<link rel="stylesheet" href="style.css">
+
+
+<div class="containr">
+
+    <div class="items item1">
+        <a class="location-title" href="ff10.php">FINAL FANTASY X</a>
+        <div class="container-img">
+            <img src="..\photos en tt genre\tidusyuna.jpg" class="img1">    
+        </div>
+    </div>
+
+    <div class="items item2">
+        <a class="location-title" href="#">FINAL FANTASY VII</a>
+        <div class="container-img2">
+            <img src="../photos en tt genre/ff7.jpg" class="img2">
+        </div>
+    </div>
+
+    <div class="items item3">
+        <a class="location-title" href="#">FINAL FANTASY II</a>
+        <div class="container-img3">
+            <img src="..\photos en tt genre\ff2.jpg" class="img3">
+        </div>
+    </div>
+
+    <div class="items item4">
+        <a class="location-title" href="#">FINAL FANTASY XII</a>
+        <div class="container-img4">
+            <img src="..\photos en tt genre\ff12-2.jpg" class="img4">
+        </div>
+    </div>
+
+    <div class="items item5">
+        <div class="container-img5">
+            <img src="..\photos en tt genre\ff4.jpg" class="img5">
+        </div>
+    </div>
+
+    <div class="items item6">
+        <div class="container-img6">
+            <img src="..\photos en tt genre\ff15.jpg" class="img6">
+        </div>
+    </div>
+
+    <div class="items item7">
+        <div class="container-img7">
+            <img src="..\photos en tt genre\ff13.jpg" class="img7">
+        </div>
+    </div>
+
+    <div class="items item8">
+        <a class="location-title" href="#">FINAL FANTASY ?</a>
+        <div class="container-img8">
+            <img src="" class="img8">
+        </div>
+    </div>
+
+    <div class="items item9">
+        <div class="container-img5">
+            <img src="../photos en tt genre/ff.jpg" class="img5">
+        </div>
+    </div>
+
+    <div class="items item10">
+        <div class="container-img5">
+            <img src="../photos en tt genre/ff.jpg" class="img5">
+        </div>
+    </div>
+
+    <div class="items item11">
+        <div class="container-img5">
+            <img src="../photos en tt genre/ff.jpg" class="img5">
+        </div>
+    </div>
+
+    <div class="items item12">
+        <div class="container-img12">
+            <img src="../photos en tt genre/ff16.jpg" class="img12">
+        </div>
+    </div>
+
+    <div class="items item13">
+        <div class="container-img5">
+            <img src="../photos en tt genre/ff.jpg" class="img5">
+        </div>
+    </div>
+
+    <div class="items item14">
+        <div class="container-img5">
+            <img src="../photos en tt genre/ff.jpg" class="img5">
+        </div>
+    </div>
+
+    <div class="items item15">
+        <div class="container-img5">
+            <img src="../photos en tt genre/ff.jpg" class="img5">
+        </div>
+    </div>
+
+    <div class="items item16">
+        <div class="container-img5">
+            <img src="../photos en tt genre/ff.jpg" class="img5">
+        </div>
+</div>
+</div>
+
+
+
+</body>
+</html>
     </body>
 </html>
