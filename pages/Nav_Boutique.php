21e078a2e641aee6358c7ed8b8a3de94b1ca6070
Campagne_Alexandre <campagne.a@hotmail.fr>
2021-04-22 15:33:19 +0200

    mise a jour du profil (non fini)

---
+++ pages/Nav_Boutique.php
@@ -0,0 +1,27 @@
+<div class="row mt-5">
+    <div class="col-12 position-nav-boutique"></div>
+    <div class="col-md-3 text-center border-end border-bottom border-2 border-danger back-image text-white position-fixed" height="100%">
+        <h2 class="pt-5 pb-3">Liste de la Boutique</h2>
+        <div class="dropdown-divider pt-3"></div>
+        <div class="dropdown-divider pb-3"></div>
+        <a class="dropdown-item pt-1 pb-3 text-white" type="button" href="">
+            <h3>Nouveauté</h3>
+        </a>
+        <div class="dropdown-divider pt-3"></div>
+        <a class="dropdown-item pt-1 pb-3 text-white" type="button" href="">
+            <h3>Jeux</h3>
+        </a>
+        <div class="dropdown-divider pt-3"></div>
+        <a class="dropdown-item pt-1 pb-3 text-white" type="button" href="">
+            <h3>Goodies</h3>
+        </a>
+        <div class="dropdown-divider pt-3"></div>
+        <a class="dropdown-item pt-1 pb-3 text-white" type="button" href="">
+            <h3>Livres</h3>
+        </a>
+        <div class="dropdown-divider pt-3"></div>
+        <a class="dropdown-item pt-1 pb-4 text-white" type="button" href="">
+            <h3>Figurines</h3>
+        </a>
+    </div>
+</div>
