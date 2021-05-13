221c20745bdbebd5e75f77b2e04454212a82f414
Tommy Vichidvongsa <tommychel@hotmail.fr>
2021-05-13 09:58:13 +0200

    maj histoire + forum

--- pages/forum.php
+++ pages/forum.php
@@ -1,21 +1,37 @@
 <?php
     include 'header.php';
-?>
-        <h1 id="forum-title">BIENVENUE SUR LE FORUM !</h1>
-            <div class="center-buttons">
-                <button type="button" class="btn btn-primary">NEW COMMENT</button>
-            </div><br><br><br>
+?>  
+        <div class="backG-color">
+            <h1 id="forum-title">BIENVENUE SUR LE FORUM !</h1>
+                <div class="center-buttons">
+                    <button type="button" class="btn btn-primary">post</button>
+                </div><br><br><br>
 
-         <table class="table">
-            <thead>
-                <tr>
+                <table class="table">
+                    <thead>
+                        <tr>
 
-                    <th scope="col">Post date</th>
-                    <th scope="col">Nickname</th>
-                    <th scope="col">Comment</th>
-                    <th scope="col">Rate comment</th>
+                            <th scope="col">Id post</th>
+                            <th scope="col">Date post</th>
+                            <th scope="col">Contenu</th>
+                            <th scope="col">Page</th>
+                            <th scope="col">Sujet</th>
+                            <th scope="col">Catégorie du sujet</th>
+                            <th scope="col">Id user</th>
+                        </tr>
+                    </thead>
 
-                </tr>
-            </thead>
+                    <tbody>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tbody>
+                </table>
+        </div>
+
     </body>
 </html>
--- pages/histoire.php
+++ pages/histoire.php
@@ -3,7 +3,7 @@ include 'header.php';
 ?>
 
 <div class="mainBlock">
-    <div class="title">L'HISTOIRE DE SQUARE</div>
+    <!-- <div class="title">L'HISTOIRE DE SQUARE</div> -->
     <!-- <div class="divider"></div> -->
     <div class="history-block topblock">
         <div class="history-pic-top">
@@ -45,10 +45,7 @@ include 'header.php';
             the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
             text ever since the 1500s<br>, when an unknown printer took a galley of type and scrambled
             it to make a type specimen book. It has survived not only five centuries,
-            but also the leap into<br> electronic typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-            text ever since the 1500s<br>, when an unknown printer took a galley of type and scrambled
-            it to make a type specimen book. It has survived not only five centuries,
+
 
         </div>
         <div class="clear"></div>
